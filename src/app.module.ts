14cf289292ecd9694925c30bfb6f353847d19afd
Qulllaia <znurock@mail.ru>
2024-11-01 20:16:48 +0300

    staticFiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,15 @@ import { PostsService } from './posts/posts.service';
 import { PostsModule } from './posts/posts.module';
 import { Post } from './posts/posts.model';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   controllers: [],
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
